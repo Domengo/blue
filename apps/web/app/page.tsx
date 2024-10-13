4b17368c20f215d6b0e4f39819fa9894ea5cb3ae
Dominic Sengo <107099599+Domengo@users.noreply.github.com>
2024-10-13 21:17:07 +0000

    feat(tailwind): integrate Tailwind CSS and update styling in layout and page components

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -7,7 +7,7 @@ import styles from "../styles/index.module.css";
 export default function Web() {
   return (
     <div className={styles.container}>
-      <h1>Webo</h1>
+      <h1 className="text-3xl font-bold underline">Webo</h1>
       <Button onClick={() => console.log("Pressed?!")} text="Boop" />
     </div>
   );
