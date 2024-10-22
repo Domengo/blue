4f57326570078692c425753d227f782ca87ddd14
Dominic Sengo <107099599+Domengo@users.noreply.github.com>
2024-10-22 23:53:54 +0000

    refactor(native): update global.d.ts, postcss.config.js, .gitignore, babel.config.js, and tsconfig.json

---
+++ packages/ui/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "../../apps/web/**/*.{js,ts,jsx,tsx}",
+    "../../apps/ww/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
